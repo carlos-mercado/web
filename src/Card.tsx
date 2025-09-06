10aef707e9ae32fa77b3eec6b81c51aa4fcdb4e7
carlos-mercado <2.carlosmercado@gmail.com>
2025-09-06 16:22:59 -0700

    small improvements to mobile

--- src/Card.tsx
+++ src/Card.tsx
@@ -9,18 +9,21 @@ interface  IconInfo{
 
 
 function Card(props:IconInfo){
+    const isMobile = /Mobi|Android/i.test(window.navigator.userAgent);
 
     const handleIconClick = (event: {detail: any;}) => {
+        isMobile ? event.detail = 2 : event.detail = event.detail;
+
         props.onIconClick([props.cardID, event.detail])
     }
-    
+
     const cardClickedStyle = {
         backgroundColor: props.isSelected ? 'rgba(0, 116, 161, 0.5)' : 'rgba(0, 116, 161, 0)',
         border: props.isSelected ? '2px solid grey' : ''
     }
 
     return(
-        <Draggable>
+        <Draggable cancel={isMobile ? ".card" : ""}>
             <div className="card" onClick={handleIconClick} style={cardClickedStyle}>
                 <img draggable="false" src={props.icon} alt="icon-picture" className="card-image"></img>
                 <p>{props.cardID}</p>
